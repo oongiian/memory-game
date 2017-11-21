411aa91d490f917130a3f94fe4dac94a2c4a0c57
dongxian <dongxian@dongxiandeMacBook-Pro.local>
2017-11-21 16:32:44 +0800

    feature:app.js添加卡片类型数组

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var deck = [
+    'fa fa-diamond', 
+    'fa fa-diamond', 
+    'fa fa-paper-plane-o',
+    'fa fa-paper-plane-o',
+    'fa fa-bolt',
+    'fa fa-bolt',
+    'fa fa-bicycle',
+    'fa fa-bicycle',
+    'fa fa-anchor',
+    'fa fa-anchor',
+    'fa fa-cube',
+    'fa fa-cube',
+    'fa fa-leaf',
+    'fa fa-leaf',
+    'fa fa-bomb',
+    'fa fa-bomb'
+]
 
 /*
  * Display the cards on the page
